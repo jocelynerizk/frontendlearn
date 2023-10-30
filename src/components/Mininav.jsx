ffb3ca3a616c97acd8864297e3c61c3acdd3d975
jocelynerizk <95102544+jocelynerizk@users.noreply.github.com>
2023-10-30 03:12:01 +0200

    juut

--- src/components/Mininav.jsx
+++ src/components/Mininav.jsx
@@ -17,7 +17,6 @@ const Mininav = () => {
       </button>
       <nav  className="header-nav"ref={navRef}>
       <a className="header-a"  href="/Home">Home</a>
-      <a className="header-a"  href="/c">Courses</a>
       <a className="header-a"  href="/#sign">Sign</a>
       <a className="header-a"  href="/#Teachers">Board</a>
         <button className="nav-btn nav-close-btn" onClick={showNavBar}>
